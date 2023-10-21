5165dc950a1c69f0c9674dff45c1e6b1a6ca48ec
financieras <financieras@gmail.com>
2023-10-21 11:54:37 +0200

    Update myfunctions.py

--- mypy/myfunctions.py
+++ mypy/myfunctions.py
@@ -1,19 +1,19 @@
 def sumar(n1,n2):
-	"""Permite sumar dos números."""
-	print(f"El resultado de la suma es {n1+n2}")
+    """Permite sumar dos números."""
+    print(f"El resultado de la suma es {n1+n2}")
 
 def restar(n1,n2):
-	"""Permite restar dos números."""
-	print(f"El resultado de la resta es {n1-n2}")
+    """Permite restar dos números."""
+    print(f"El resultado de la resta es {n1-n2}")
 
 def multiplicar(n1,n2):
-	'''Permite multiplicar dos números.'''
-	print(f"El resultado del producto es {n1*n2}")
+    '''Permite multiplicar dos números.'''
+    print(f"El resultado del producto es {n1*n2}")
 
 def dividir(n1,n2):
-	"""Permite dividir dos números.
-	Si el denominador es cero se avisa."""
-	if n2:
-		print(f"El resultado de la división es {n1/n2}")
-	else:
-		print("División por Cero")
+    """Permite dividir dos números.
+    Si el denominador es cero se avisa."""
+    if n2:
+        print(f"El resultado de la división es {n1/n2}")
+    else:
+        print("División por Cero")
